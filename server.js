ee79a25feefb89dcf008fa44fa6c67d5fdd3e8ce
Saurav Acharya <sacharavya@gmail.com>
2021-03-13 17:23:28 +0545

    changes addes

--- server.js
+++ server.js
@@ -12,16 +12,13 @@ const transporter = nodemailer.createTransport({
 })
 
 const users = [
+  'sacharavya@gmail.com',
   'sacharavya1@gmail.com',
   'sacharavya2@gmail.com',
   'sacharavya3@gmail.com',
-  'sacharavya@gmail.com',
-  'nepalimohan@outlook.com',
-  'bsomeman326@gmail.com',
-  'upretiyummiee@hotmail.com',
 ]
 
-users.forEach((to, i, email) => {
+users.forEach((to, i, msg) => {
   const mailOptions = {
     from: process.env.EMAIL,
     subject: 'Trying Nodemailer in my nodejs app',
